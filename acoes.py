94bcc5eee8e1cdbc28b55f4811f204bb2e52b02d
Carlos Matos <carlinhos12345678962@gmail.com>
2023-05-03 15:36:23 -0300

    add Data

--- acoes.py
+++ acoes.py
@@ -2,6 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 import mysql.connector
+from datetime import date
+
+data = date.today()
+
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -22,10 +26,12 @@ def mostraTabela():
 def varrePagina(qtd):
     i = 1
     j = 0
+    pagina = 1
     driver = webdriver.Edge()
     driver.get("https://www.infomoney.com.br/cotacoes/b3/acao/")
     sleep(3)
     for j in range(0, qtd):
+        print(f"Pagina {pagina}")
         for i in range(1, 16):
 
             nome = driver.find_element(By.XPATH, f"/html/body/div[5]/article/div[3]/div[2]/div[2]/div[1]/table/tbody/tr[{i}]/td[1]/a/strong").text
@@ -38,10 +44,12 @@ def varrePagina(qtd):
                 varD = float(varD.replace(",", ".").replace("%", "")) #Tranformando em float para inserir na tabela
             except:
                 varD = 0
+            try:
+                var12M = driver.find_element(By.XPATH, f"/html/body/div[5]/article/div[3]/div[2]/div[2]/div[1]/table/tbody/tr[{i}]/td[4]/span").text
+                var12M = float(var12M.replace(",", ".").replace("%", "")) #Tranformando em float para inserir na tabela
+            except:
+                var12M = 0
             
-            var12M = driver.find_element(By.XPATH, f"/html/body/div[5]/article/div[3]/div[2]/div[2]/div[1]/table/tbody/tr[{i}]/td[4]/span").text
-            var12M = float(var12M.replace(",", ".").replace("%", "")) #Tranformando em float para inserir na tabela
-
             volume = driver.find_element(By.XPATH, f"/html/body/div[5]/article/div[3]/div[2]/div[2]/div[1]/table/tbody/tr[{i}]/td[5]/span").text
             #Tranformando em float e multiplicando em milhão ou bilhão para inserir na tabela
             ultimaLetra = volume[-1]
@@ -53,13 +61,13 @@ def varrePagina(qtd):
                 volume = float(numeroTexto) * 1000000000
             else:
                 volume = float(numeroTexto)
-
-            print(f"Nome: {nome} | Preço: R$ {preco} | Var D: {varD}% | Var 12M: {var12M}% | Volume: {volume}")
-            sql = f"INSERT INTO acoes (nome, preco, varD, var12M, volume) VALUES ('{nome}', {preco}, {varD}, {var12M}, {volume})"
+            print(f"Nome: {nome} | Data: {data} | Preço: R$ {preco} | Var D: {varD}% | Var 12M: {var12M}% | Volume: {volume}")
+            sql = f"INSERT INTO acoes (nome, data_, preco, varD, var12M, volume) VALUES ('{nome}', '{data}', {preco}, {varD}, {var12M}, {volume})"
             cursor.execute(sql)
             
         proximo = driver.find_element(By.XPATH, "/html/body/div[5]/article/div[3]/div[2]/div[2]/div[2]/div/button[2]")
         driver.execute_script("arguments[0].click();", proximo)
+        pagina +=1
         mydb.commit()
         sleep(5)
 
